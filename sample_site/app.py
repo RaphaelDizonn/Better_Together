6c10bb503b76643e5dc03ed8a7788a11f25925e1
RaphaelDizonn <90470101+RaphaelDizonn@users.noreply.github.com>
2022-03-27 21:43:37 -0400

    test

--- sample_site/app.py
+++ sample_site/app.py
@@ -1,14 +1,25 @@
-from flask import Flask 
-from flask_socketio import SocketIO, send
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mysecret'
-socketio = SocketIO(app, cors_allowed_origins='*')
 
-@socketio.on('message')
-def handleMessage(msg):
-	print('Message: ' + msg)
-	send(msg, broadcast=True)
+@app.route("/")
+def index():
 
-if __name__ == '__main__':
-	socketio.run(app)
+    # Load current count
+    f = open("count.txt", "r")
+    count = int(f.read())
+    f.close()
+
+    # Increment the count
+    count += 1
+
+    # Overwrite the count
+    f = open("count.txt", "w")
+    f.write(str(count))
+    f.close()
+
+    # Render HTML with count variable
+    return render_template("index.html", count=count)
+
+if __name__ == "__main__":
+    app.run()
